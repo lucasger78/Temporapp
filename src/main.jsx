79f0e751eef61bf005b102446504bb13b606ac7e
lucasger78 <lucasger78@hotmail.com>
2025-04-29 12:31:11 -0300

    actualizacion interna

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { WeatherApp } from './weatherApp'
+import { WeatherApp } from './WeatherApp'
 import './style.css'
+
 
 
 createRoot(document.getElementById('root')).render(
