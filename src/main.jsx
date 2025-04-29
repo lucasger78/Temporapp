3dfb12b622eef97b0e5edf6833d81d7831df7a08
lucasger78 <lucasger78@hotmail.com>
2025-04-29 12:07:25 -0300

    app del clima en react subida

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { WeatherApp } from './weatherApp'
+import './style.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <WeatherApp/>
+  </StrictMode>,
+)
