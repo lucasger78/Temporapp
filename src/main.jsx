5c064963a0698d2684ff92196f826ae0a737a929
lucasger78 <lucasger78@hotmail.com>
2025-04-29 16:24:34 -0300

    actualizacion para que no renderice el background cada vez

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { WeatherApp } from './weatherApp'
+import { WeatherApp } from './WeatherApp'
 import './style.css'
 
 
